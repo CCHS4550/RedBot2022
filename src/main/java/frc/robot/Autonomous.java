a24c513196ef3bb8e1ed75e9436d4acee34b0033
CCHS4550 <60554548+CCHS4550@users.noreply.github.com>
2022-12-06 17:33:37 -0700

    Revert "poggers!"
    This reverts commit e57661df0aa945bff385168728ec3e61ff2afcd2.

---
+++ src/main/java/frc/robot/Autonomous.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class Autonomous extends SequentialCommandGroup{
+    //auto is set up such that it will run when you want it to
+
+    //inside the constructor you have to put an object of each subsystem you plan to use
+    public Autonomous(DriveTrain chassis, Shooter shooter){
+        //put all commands within this super.addcommands
+        //make note that it uses commas instead of semicolons because you're technically adding them in a list
+        super.addCommands(
+            //whatever you put here must extend from command
+            //https://first.wpi.edu/wpilib/allwpilib/docs/release/java/edu/wpi/first/wpilibj2/command/package-summary.html
+            //check link for relevant subclasses 
+            chassis.turnAngle(45),
+            chassis.moveTo(7),
+            chassis.turnAngle(-90),
+            shooter.shootForTime(0.5, 3)
+        );
+    }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,74 @@
+package frc.robot;
+
+import javax.print.attribute.standard.JobHoldUntil;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.helpers.OI;
+import frc.parent.ControlMap;
+import frc.robot.subsystems.*;
+public class RobotContainer {
+    //must instantiate an object of each subsystem you use
+    private DriveTrain chassis = new DriveTrain();
+    private Arms arms = new Arms();
+    private Shooter shooter = new Shooter();
+
+    Joystick[] controllers = OI.joystickArray;
+
+    public RobotContainer(){
+        configureButtons();
+        chassis.setDefaultCommand(new RunCommand(() -> chassis.axisDrive(OI.axis(0, ControlMap.L_JOYSTICK_VERTICAL), OI.axis(0, ControlMap.R_JOYSTICK_HORIZONTAL)), chassis));
+        shooter.setDefaultCommand(new RunCommand(() -> shooter.shoot(OI.axis(1, ControlMap.RT)), shooter));
+        // arms.setDefaultCommand(new RunCommand(() -> arms.setSpeed(OI.dPadAng(1) > -1 ? Math.cos(Math.toRadians(OI.dPadAng(1))) : 0), arms));
+        
+        //arms.setDefualtCommand(new RunCommand(() -> arms.setSpeed(OI.dPad(1, )), arms);
+    } 
+        //This is Tyler's contribution to this code, you're welcome!!!
+    private void configureButtons() {
+        //this is how you do teleop stuff
+        //when you create a trigger or trigger subclass, it will run whatever you want run when you specify
+        
+        //https://first.wpi.edu/wpilib/allwpilib/docs/release/java/edu/wpi/first/wpilibj2/command/button/Button.html 
+        //list of modifiers to control what happens for trigger objects (joystickbuttons extend trigger)
+
+        //https://first.wpi.edu/wpilib/allwpilib/docs/release/java/edu/wpi/first/wpilibj2/command/button/Trigger.html 
+        //triggers are like buttons, but you can control when they go off
+        //any logic amongst triggers must be done with .and, .negate, and others
+        //see link for full list of logic operators
+
+        // final double SHOOTER_INACTIVE_VALUE = 0.0;
+        // final double SHOOTER_ACTIVE_VALUE = 0.5;
+        // final double ACTIVE_ANALOG_CONSTANT = .15;
+
+
+        new JoystickButton(controllers[0], ControlMap.A_BUTTON)
+         .whenPressed(() -> chassis.toggleFastMode());
+
+        new JoystickButton(controllers[1], ControlMap.A_BUTTON)
+         .whenPressed(() -> arms.toggleArms());
+
+    //     new Trigger(){
+    //         @Override
+    //         public boolean get(){
+    //             return OI.axis(1, ControlMap.RT)  > ACTIVE_ANALOG_CONSTANT;
+    //         }
+    //     }.whenActive(() -> shooter.shoot(SHOOTER_ACTIVE_VALUE))
+    //     .whenInactive(() -> shooter.shoot(SHOOTER_INACTIVE_VALUE));
+    }
+        
+
+
+        
+
+    void test(){
+
+    }
+
+    public Command getAutoCommand(){
+        //see Autonomous class for more details
+        return new Autonomous(chassis, shooter);
+    }
+}
